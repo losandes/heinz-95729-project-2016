42adac8ceaa552ac6347e8db3e1dea2c5f7628a0
Ankush Grover <simplyankush@gmail.com>
2014-11-29 13:18:23 -0500

    Added checkoutController
    doesnt work yet but doesnt break soucre either

---
+++ Moviq/Js/controllers/checkoutController.js
@@ -0,0 +1,18 @@
+﻿/*global define, JSON*/
+
+define('controllers/checkoutController', {
+    init: function ($, routes, viewEngine) {
+    "use strict";
+
+    
+    routes.get('/^\/#\/checkout\/?/i', function (context) {
+        viewEngine.setView({
+            template: 't-register',
+            data: {}
+        });
+    });
+
+  
+
+}
+});
