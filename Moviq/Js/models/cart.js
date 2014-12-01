6c803e67a633813f3d67ea64702dd8c0b09f7b98
Ankush Grover <simplyankush@gmail.com>
2014-12-01 14:02:15 -0500

    Cart and CartItem

--- Moviq/Js/models/cart.js
+++ Moviq/Js/models/cart.js
@@ -1,1 +1,54 @@
-function createItem()
+/*jslint plusplus: true*/
+/*global define*/
+define('models/cart', { init: function (ko, CartItem) {
+    "use strict";
+    
+    if (!ko) {
+        throw new Error('Argument Exception: ko is required to init the books module');
+    }
+    
+    if (!CartItem) {
+        throw new Error('Argument Exception: CartItem is required to init the books module');
+    }
+    
+    var Cart;
+
+    Cart = function (cart) {
+        var $this = this;
+
+        $this.cart = ko.observableArray();
+
+        $this.addCartItem = function (cartItem) {
+            if (!cartItem) {
+                throw new Error('Argument Exception: the argument, book, must be defined to add a book');
+            }
+
+            var i = 0;
+
+            if (!(cartItem instanceof CartItem)) {
+                cartItem = new CartItem(cartItem);
+            }
+
+            $this.cart.push(cartItem);
+        };
+
+        $this.addCart = function (cart) {
+            if (!cart) {
+                throw new Error('Argument Exception: the argument, books, must be defined to add books');
+            }
+
+            var i = 0;
+
+            for (i; i < cart.length; i++) {
+                $this.addCartItem(cart[i]);
+            }
+        };
+
+        if (cart) {
+            $this.addCart(cart);
+        }
+    };
+
+    return Cart;
+}
+});
--- Moviq/Js/models/cartItem.js
+++ Moviq/Js/models/cartItem.js
@@ -1,1 +1,25 @@
-﻿
+﻿/*global define*/
+define('models/cartItem', { init: function (ko, Product) {
+    "use strict";
+
+    if (!ko) {
+        throw new Error('Argument Exception: ko is required to init the book module');
+    }
+    
+    if (typeof Product !== 'function') {
+        throw new Error('Argument Exception: Product is required to init the book module');
+    }
+   
+    var CartItem = function (cartItem) {
+        var $this = this;
+        cartItem = cartItem || {};
+        
+        $this.setProductData($this, cartItem);
+     
+       };
+
+    CartItem.prototype = new Product();
+
+    return CartItem;
+}
+});
