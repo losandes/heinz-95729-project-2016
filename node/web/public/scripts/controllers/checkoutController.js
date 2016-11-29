962459f99faffc09c19cda306cb85a465dba54c1
sneha <snehapoddarnew@gmail.com>
2016-11-29 09:12:15 -0500

    changes in checkout controller

--- node/web/public/scripts/controllers/checkoutController.js
+++ node/web/public/scripts/controllers/checkoutController.js
@@ -1,7 +1,7 @@
 Hilary.scope('heinz').register({
     name: 'checkoutController',
-    dependencies: ['newGidgetModule', 'GidgetRoute', 'locale', 'viewEngine','Checkout','Products', 'jQuery'],
-    factory: function ($this, GidgetRoute, locale, viewEngine, Checkout, Products, $) {
+    dependencies: ['newGidgetModule', 'GidgetRoute', 'locale', 'viewEngine','Checkouts','Products', 'jQuery'],
+    factory: function ($this, GidgetRoute, locale, viewEngine, Checkouts, Products, $) {
         'use strict';
         
         $this.get['/checkout'] = function () {
@@ -10,21 +10,29 @@ Hilary.scope('heinz').register({
             });
         };
         
-        /*
+        
         $this.get['/checkout/:email'] = new GidgetRoute({
             routeHandler: function (err, req) {
                 $.ajax({
-                    url: '/api/checkout/' + req.params.uid
+                    url: '/api/checkout/:email' + req.uri.query.q,
+                    method: 'GET'
                 }).done(function (data) {
-                    var checkout = new Checkout(data);
+                    var results = new Checkouts(data);
 
-                    viewEngine.setVM({
-                        template: 't-checkout',
-                        data: { checkout: checkout }
-                    });
+                    if (results.length > 0) {
+                        viewEngine.setVM({
+                            template: 't-checkout',
+                            data: results
+                        });
+                    } else {
+                        viewEngine.setVM({
+                            template: 't-no-results',
+                            data: { searchterm: req.uri.query.q }
+                        });
+                    }
                 });
             }
-        });*/
+        });
         
 
         // GET /#/search/?q=searchterm
