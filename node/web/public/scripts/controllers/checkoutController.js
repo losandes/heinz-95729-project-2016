adb344ae3b012540d70307210376bee0f724796e
sneha <snehapoddarnew@gmail.com>
2016-11-29 09:35:56 -0500

    removing the search function from checkout controller

--- node/web/public/scripts/controllers/checkoutController.js
+++ node/web/public/scripts/controllers/checkoutController.js
@@ -37,28 +37,7 @@ Hilary.scope('heinz').register({
 
         // GET /#/search/?q=searchterm
         // search for products
-        $this.get['/search'] = new GidgetRoute({
-            routeHandler: function (err, req) {
-                $.ajax({
-                    url: '/api/search?q=' + req.uri.query.q,
-                    method: 'GET'
-                }).done(function (data) {
-                    var results = new Products(data);
 
-                    if (results.products().length > 0) {
-                        viewEngine.setVM({
-                            template: 't-product-grid',
-                            data: results
-                        });
-                    } else {
-                        viewEngine.setVM({
-                            template: 't-no-results',
-                            data: { searchterm: req.uri.query.q }
-                        });
-                    }
-                });
-            }
-        });
 
         return $this;
     }
