419180a0b55de9411cd008a0cd69c4ec4b99c56a
sneha <snehapoddarnew@gmail.com>
2016-11-28 22:22:53 -0500

    merge conflict error resolve

--- node/web/api/index.js
+++ node/web/api/index.js
@@ -1,8 +1,7 @@
 module.exports = [
     require('./searchApiController.js'),
     require('./booksApiController.js'),
-	require('./checkoutApiController.js')
-
+	require('./checkoutApiController.js'),
     require('./addToCartApiController.js')
 
 
