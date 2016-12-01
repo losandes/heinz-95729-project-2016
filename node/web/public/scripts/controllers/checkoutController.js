00d81bdf1e7b293f40fd7111da19f4d7bcb71d7c
sneha <snehapoddarnew@gmail.com>
2016-12-01 04:09:05 -0500

    fetching data from DB is working + added paymen Modal

--- node/web/public/scripts/controllers/checkoutController.js
+++ node/web/public/scripts/controllers/checkoutController.js
@@ -1,7 +1,7 @@
 Hilary.scope('heinz').register({
     name: 'checkoutController',
-    dependencies: ['newGidgetModule', 'GidgetRoute', 'locale', 'viewEngine','Checkouts','Products', 'jQuery'],
-    factory: function ($this, GidgetRoute, locale, viewEngine, Checkouts, Products, $) {
+    dependencies: ['newGidgetModule', 'GidgetRoute', 'locale', 'viewEngine','Checkout', 'Checkouts','Products', 'jQuery'],
+    factory: function ($this, GidgetRoute, locale, viewEngine, Checkout, Checkouts, Products, $) {
         'use strict';
         
         $this.get['/checkout'] = function () {
@@ -14,26 +14,21 @@ Hilary.scope('heinz').register({
         $this.get['/checkout/:email'] = new GidgetRoute({
             routeHandler: function (err, req) {
                 $.ajax({
-                    url: '/api/checkout/:email' + req.uri.query.q,
-                    method: 'GET'
+                    url: '/api/checkout/' + req.params.email
                 }).done(function (data) {
-                    var results = new Checkouts(data);
+                    var checkout = new Checkout(data);
+                    console.log(checkout);
+                    viewEngine.setVM({
+                        template: 't-checkout',
+                        data: { checkout: checkout }
+                    });
+                    
 
-                    if (results.length > 0) {
-                        viewEngine.setVM({
-                            template: 't-checkout',
-                            data: results
-                        });
-                    } else {
-                        viewEngine.setVM({
-                            template: 't-no-results',
-                            data: { searchterm: req.uri.query.q }
-                        });
-                    }
                 });
             }
         });
         
+
 
         // GET /#/search/?q=searchterm
         // search for products
