161aa959df73def0b86360d8154fe68045526def
sneha <snehapoddarnew@gmail.com>
2016-12-04 20:15:01 -0500

    small fix

--- node/web/api/index.js
+++ node/web/api/index.js
@@ -2,9 +2,9 @@ module.exports = [
     require('./searchApiController.js'),
     require('./booksApiController.js'),
 	require('./checkoutApiController.js'),
-	require('./errorApiController.js')
-	require('./addToCartApiController.js'),
-	require('./userApiController.js'),
+	require('./errorApiController.js'),
+	//require('./addToCartApiController.js'),
+	//require('./userApiController.js'),
 	require('./errorApiController.js'),
 	require('./orderHistoryApiController.js')
 
