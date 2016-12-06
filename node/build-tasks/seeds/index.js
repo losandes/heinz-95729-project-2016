9186c662d593a7afb501288ef0c290c3632f3f8e
sneha <snehapoddarnew@gmail.com>
2016-12-06 10:18:25 -0500

    order details creating backend controller

--- node/build-tasks/seeds/index.js
+++ node/build-tasks/seeds/index.js
@@ -2,7 +2,6 @@ module.exports = {
     products: require('./books.json'),
     users: require('./users.json'),
     checkout: require('./checkout.json'),
-
-
+    orderDetails: require('./orderHistory.json'),
     orderHistory: require('./orderHistory.json')
 };
