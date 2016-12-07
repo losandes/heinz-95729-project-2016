7b69d41f10716413d137e964ebddad825e75a0e2
sneha <snehapoddarnew@gmail.com>
2016-12-06 22:33:40 -0500

    passing book id

--- node/web/public/scripts/checkoutPage.js
+++ node/web/public/scripts/checkoutPage.js
@@ -30,13 +30,15 @@ function removeItem(removeButton)
 {
   /* Remove row from DOM and recalc cart total */
   var productRow = $(removeButton).parent().parent();
+  var uid = productRow.find('#uid').text();
+  $.post('/removeCart', {'uid': uid});
+  
   productRow.slideUp(fadeTime, function() {
     productRow.remove();
     recalculateCart();
   });
 
-	var uid = document.getElementById("uid").innerHTML;
-    $.post('/removeCart', {'uid': uid});
+	
 }
 
 function directToCheckout()
